d47b60e2b16c492532539b33a747ef7e36690ebc
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2025-05-04 13:05:26 +0200

    Update setup.sh

--- lab00_setup/setup.sh
+++ lab00_setup/setup.sh
@@ -2,11 +2,14 @@
 sudo apt-get update
 sudo apt-get install -y jq net-tools curl git wget vim
 
-## Install Python 3.11 and pip
+## Install Python 3.12 and pip
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get install -y python3.12
 sudo apt-get install -y python3-pip
 sudo apt-get  install python3.12-venv
+
+## Install npm
+sudo apt-get install npm
 
 ## Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
