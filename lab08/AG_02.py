1539a763ba721fb1f7e56eecf751cbe84a979629
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2025-05-04 22:16:23 +0200

    Create AG_02.py

---
+++ lab08/AG_02.py
@@ -0,0 +1,42 @@
+import asyncio
+
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.base import TaskResult
+from autogen_agentchat.conditions import ExternalTermination, TextMentionTermination
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.ui import Console
+from autogen_core import CancellationToken
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+# Create an OpenAI model client.
+model_client = OpenAIChatCompletionClient(
+    model="gpt-4o-2024-08-06",
+    # api_key="sk-...", # Optional if you have an OPENAI_API_KEY env variable set.
+)
+
+# Create the primary agent.
+primary_agent = AssistantAgent(
+    "primary",
+    model_client=model_client,
+    system_message="You are a helpful AI assistant.",
+)
+
+# Create the critic agent.
+critic_agent = AssistantAgent(
+    "critic",
+    model_client=model_client,
+    system_message="Provide constructive feedback. Respond with 'APPROVE' to when your feedbacks are addressed.",
+)
+
+# Define a termination condition that stops the task if the critic approves.
+text_termination = TextMentionTermination("APPROVE")
+
+# Create a team with the primary and critic agents.
+team = RoundRobinGroupChat([primary_agent, critic_agent], termination_condition=text_termination)
+
+async def main():
+    result = await team.run(task="Write a short poem about the fall season.")
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
