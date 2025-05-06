f8159f1079f1b86fd548b880c29fd8aced1aca6d
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2025-05-06 22:55:49 +0200

    Update chat_01.py

--- lab01/chat_01.py
+++ lab01/chat_01.py
@@ -1,12 +1,28 @@
 from openai import OpenAI
 client = OpenAI()
 
-completion = client.chat.completions.create(
+response = client.chat.completions.create(
   model="gpt-4o",
   messages=[
-    {"role": "developer", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "What is AI ?"}
-  ]
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "what is AI ?"
+        }
+      ]
+    }
+  ],
+  response_format={
+    "type": "text"
+  },
+  temperature=1,
+  max_completion_tokens=2048,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  store=False
 )
 
-print(completion.choices[0].message.content)
+print(client.choices[0].message.content)
