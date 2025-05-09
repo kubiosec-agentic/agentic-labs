c284af76c565eb9c26fb8714a9c1ad058722aad1
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2025-05-10 01:28:54 +0200

    Update mcp_02.py

--- lab07/mcp_02.py
+++ lab07/mcp_02.py
@@ -18,32 +18,27 @@ async def run(mcp_server: MCPServer):
         model_settings=ModelSettings(tool_choice="required"),
     )
 
-    # Use the `add` tool to add two numbers
-    message = "Add these numbers: 7 and 22."
-    print(f"Running: {message}")
-    result = await Runner.run(starting_agent=agent, input=message)
-    print(result.final_output)
+    test_messages = [
+        "Add these numbers: 7 and 22.",
+        "What's the weather in Tokyo?",
+        "What's the secret word?",
+    ]
 
-    # Run the `get_weather` tool
-    message = "What's the weather in Tokyo?"
-    print(f"\n\nRunning: {message}")
-    result = await Runner.run(starting_agent=agent, input=message)
-    print(result.final_output)
+    for idx, message in enumerate(test_messages):
+        print(f"\n\nRunning: {message}" if idx > 0 else f"Running: {message}")
+        result = await Runner.run(starting_agent=agent, input=message)
+        print(result.final_output)
 
-    # Run the `get_secret_word` tool
-    message = "What's the secret word?"
-    print(f"\n\nRunning: {message}")
-    result = await Runner.run(starting_agent=agent, input=message)
-    print(result.final_output)
+
+async def init_server():
+    return MCPServerSse(
+        name="SSE Python Server",
+        params={"url": "http://localhost:8000/sse"},
+    )
 
 
 async def main():
-    async with MCPServerSse(
-        name="SSE Python Server",
-        params={
-            "url": "http://localhost:8000/sse",
-        },
-    ) as server:
+    async with await init_server() as server:
         trace_id = gen_trace_id()
         with trace(workflow_name="SSE Example", trace_id=trace_id):
             print(f"View trace: https://platform.openai.com/traces/trace?trace_id={trace_id}\n")
