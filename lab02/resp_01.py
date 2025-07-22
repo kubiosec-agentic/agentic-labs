251585d1404a2ffce46d6cdf6e208e4d146405b0
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2025-07-22 21:24:10 +0200

    Update resp_01.py

--- lab02/resp_01.py
+++ lab02/resp_01.py
@@ -1,12 +1,14 @@
 from openai import OpenAI
 client = OpenAI()
 
+print("----First Response----")
 response = client.responses.create(
     model="gpt-4o-mini",
     input="tell me a joke",
 )
 print(response.output_text)
 
+print("----Second Response----")
 second_response = client.responses.create(
     model="gpt-4o-mini",
     previous_response_id=response.id,
