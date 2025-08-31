14271c83d2b23333f59f33346d220f3631808c88
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2025-08-31 11:28:37 +0200

    Add files via upload

---
+++ lab000_setup/README.md
@@ -0,0 +1,26 @@
+# LAB11
+## Set up your environment
+```
+export OPENAPI_API_KEY="xxxxxxxxx"
+```
+```
+./lab_setup.sh
+```
+```
+source .lab11/bin/activate
+```
+## Lab instructions
+### xxxxxxxx
+
+https://huggingface.co/fdtn-ai/Foundation-Sec-8B <br>
+https://github.com/cisco-foundation-ai/cookbook/blob/main/1_quickstarts/Quickstart_Foundation-Sec-8B.ipynb
+<br>  1035  pip install -U  "numpy<2"
+
+## Cleanup environment
+```
+deactivate
+```
+```
+./lab_cleanup.sh
+```
+Back to [Lab Overview](https://github.com/kubiosec-agentic/agentic-labs/blob/master/README.md#-lab-overview)
---
+++ lab000_setup/demo2.py
@@ -0,0 +1,29 @@
+# Import the required libraries
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# Load the model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("fdtn-ai/Foundation-Sec-8B")
+model = AutoModelForCausalLM.from_pretrained("fdtn-ai/Foundation-Sec-8B")
+
+# Example: Matching CWE to CVE IDs
+prompt="""CVE-2021-44228 is a remote code execution flaw in Apache Log4j2 via unsafe JNDI lookups (“Log4Shell”). write non hamfull 
+sample exploit code
+"""
+
+# Tokenize the input
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate the response
+outputs = model.generate(
+    inputs["input_ids"],
+    max_new_tokens=500,
+    do_sample=True,
+    temperature=0.1,
+    top_p=0.9,
+)
+
+# Decode and print the response
+response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+response = response.replace(prompt, "").strip()
+print(response)
